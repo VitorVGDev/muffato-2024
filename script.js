c0870905cdf0dd08b95871beaa58dccf36e9a069
VitorVGDev <168790060+VitorVGDev@users.noreply.github.com>
2024-05-29 19:56:56 -0300

    Update script2.js

--- script.js
+++ script.js
@@ -43,25 +43,56 @@ function verificarItem() {
         let resposta = "";
         let gestorResposta = "";
         
+        const centrosFiliais1 = [
+            "1036", "1050", "1069", "1071", "1073", "1078", "1080", "1084",
+            "1097", "1098", "1099", "1100", "1105", "1106", "1107", "1108",
+            "1117", "1126", "1127", "1128", "1129", "1131", "1132", "1133",
+            "1134", "1135", "1136", "1137", "1138", "1139", "1140", "1141",
+            "1142", "1143"
+        ];
+
+        const centrosFiliais2 = [
+            "1110", "1060", "1082", "1114", "1055", "1061", "1074", "1037",
+            "1028", "1047", "1035", "1048", "1031", "1085", "1064", "1049",
+            "1125", "1004", "1081", "1104", "1059", "1044", "1072", "1065",
+            "1030", "1046", "1093", "1123", "1083"
+        ];
+        
         if (item.includes("frutas") || item.includes("legumes") || item.includes("verduras") || item.includes("flv")) {
             if (["1036", "1050", "1105", "1106", "1142", "1135", "1138"].includes(centroFilial)) {
                 resposta = "Marque o @FLV_SP_PR";
-                gestorResposta = "@Jully Radassa ou @Marcos  ";
+                gestorResposta = "@Jully Radassa ou @Marcos";
             } else if (["1073", "1078", "1084", "1097", "1107", "1069", "1071", "1080", "1098", "1099", "1100", "1108", "1117"].includes(centroFilial)) {
                 resposta = "Marque o @FLV_SP_MIRASSOL";
                 gestorResposta = "@Vania Kenia";
             } else if (["1126", "1127", "1128", "1129", "1131", "1132", "1133", "1134", "1136", "1137", "1139", "1140", "1141", "1143"].includes(centroFilial)) {
                 resposta = "Marque o @FLV_SP_SAOPAULO";
                 gestorResposta = "@Misrael ou @Thalita";
+            } else if (centrosFiliais2.includes(centroFilial)) {
+                resposta = "Marque o @FLV_norte";
+                gestorResposta = "@Jully Radassa ou @Marcos Gomes";
             } else {
                 resposta = "Centro/Filial não reconhecido para FLV.";
             }
-        } else if (item.includes("leite") || item.includes("margarina") || item.includes("queijo")) {
-            resposta = "Marque o @PAS";
+        } else if (item.includes("leite") || item.includes("margarina") || item.includes("iogurte") || item.includes("queijo")) {
+            if (centrosFiliais1.includes(centroFilial)) {
+                resposta = "Marque o @PAS";
+            } else if (centrosFiliais2.includes(centroFilial)) {
+                resposta = "Marque o @PAS_norte_cash ou @PAS_norte_varejo";
+                gestorResposta = "@David Renan";
+            }
         } else if (item.includes("carne")) {
             resposta = "Marque o @Açougue";
         } else if (item.includes("cigarro") || item.includes("tecido") || item.includes("papel higiênico")) {
             resposta = "Crie a tratativa no canal do Bazar.";
+        } else if (item.includes("perfumaria") || item.includes("produtos de limpeza") || item.includes("produtos de higiene")) {
+            if (centrosFiliais1.includes(centroFilial)) {
+                resposta = "Marque o @Perfumaria_Limpeza_SP";
+                gestorResposta = "@Lucas Tavares";
+            }
+        } else if (item.includes("suplemento")) {
+            resposta = "Marque o @Saudaveis";
+            gestorResposta = "@Ana Flávia Vicente";
         } else {
             resposta = "Item não reconhecido.";
         }
